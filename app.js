2ba10e8c8a49cc6b16f429a37c9e5d78ce63eb93
Pavel Prorovsky <prorovsky@gmail.com>
2017-09-12 20:55:55 +0300

    rework app

--- app.js
+++ app.js
@@ -1,7 +1,63 @@
-table = document.querySelector("table");
+"use strict";
 
-table.addEventListener("click", e => {
-    if(e.target.tagName == "TD"){
-        console.log(e);
+const table = document.querySelector("table"),
+    cells = document.querySelectorAll("td"),
+    playerControls = document.querySelector(".player-select"),
+    sideX = document.getElementById("X"),
+    sideO = document.getElementById("O"),
+    resetButton = document.getElementById("reset"),
+    startButton = document.getElementById("start"),
+    board = Array.from(Array(9).keys()),
+    winningPosition = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 4, 2]
+    ];
+
+let isGameStart = false;
+
+startButton.addEventListener("click", startGame);
+resetButton.addEventListener("click", resetState);
+
+function startGame() {
+    isGameStart = true;
+    checkGameStart(isGameStart);
+    prepareTable();
+}
+
+function prepareTable() {
+    table.addEventListener("click", giveTdValue);
+}
+
+function giveTdValue(e) {
+    if(e.target.tagName === "TD") {
+        setValueToCell(e.target, checkPlayerSide());
     }
-});
+}
+
+function checkPlayerSide() {
+    return sideX.checked ? "X" : "O"; 
+}
+
+function setValueToCell(cell, playerSide) {
+    cell.innerText = playerSide;
+}
+
+function checkGameStart(gameState) {
+    gameState ? playerControls.style.display = "none" : playerControls.style.display = "block";
+}
+
+function resetState() {
+    isGameStart = false;
+    table.removeEventListener("click", giveTdValue);
+    checkGameStart();
+    cells.forEach(cell => cell.innerText = "");
+}
+
+// messages for start and and battle like lets fight, decent opponent, I will crush you
+// better luck next time, I love you, send nudes
